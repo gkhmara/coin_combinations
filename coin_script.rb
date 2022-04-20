9da4d3bb7616e132abd61b864c6f03f0ba8da9f5
Greg Khmara <gkhmara@gmail.com>
2022-04-19 20:35:10 -0700

    updated code and added tests for code.
    co-authored-by: Justice Douglas <justice.douglas@hey.com>
    co-authored-by: Rian Carter <R.Carter86@hotmail.com>

--- coin_script.rb
+++ coin_script.rb
@@ -2,19 +2,5 @@
 
 require ('./lib/coin_logic.rb')
 
-# puts example_until_loop()
-
-# userInput = BigMunniez.new
-# puts userInput.initialize(10, 20)
-
-# animalOne = Cat.new
-# newAnimal = animalOne.initialize("Greg", 100, "Cat", "2020")
-# puts newAnimal
-
-# dave = Friend.new("Dave Smith")
-# puts dave.name
-
-# amount = BigMunniez.new(20)
-# puts amount.user_input
-
-puts change_machine(10.41)
+customer1 = Make_Munny.new
+puts customer1.change_machine(customer1.user_input)
--- spec/coin_logic_spec.rb
+++ spec/coin_logic_spec.rb
@@ -1,2 +1,36 @@
 require('rspec')
-require('ruby_logic')
+require('coin_logic')
+
+describe Make_Munny do
+  describe '#change_machine' do
+  it("should take in 1.00 and return 4 quarters") do
+    change = Make_Munny.new
+    customer1 = change.change_machine(1.00)
+    expect(customer1).to(eq("quarters = 4 | dimes = 0 | nickels = 0 | pennies = 0 | remainder = 0"))
+  end
+
+  it("should take in 0.20 and return 2 dimes") do
+    change = Make_Munny.new
+    customer1 = change.change_machine(0.20)
+    expect(customer1).to(eq("quarters = 0 | dimes = 2 | nickels = 0 | pennies = 0 | remainder = 0"))
+  end
+
+  it("should take in 0.05 and return 1 nickel") do
+    change = Make_Munny.new
+    customer1 = change.change_machine(0.05)
+    expect(customer1).to(eq("quarters = 0 | dimes = 0 | nickels = 1 | pennies = 0 | remainder = 0"))
+  end
+
+  it("should take in 0.04 and return 4 pennies") do
+    change = Make_Munny.new
+    customer1 = change.change_machine(0.04)
+    expect(customer1).to(eq("quarters = 0 | dimes = 0 | nickels = 0 | pennies = 4 | remainder = 0"))
+  end
+
+  it("should take in an amount of money and return how many quarters, dimes, nickels, and pennies") do
+      change = Make_Munny.new
+      customer1 = change.change_machine(10.00)
+      expect(customer1).to(eq("quarters = 40 | dimes = 0 | nickels = 0 | pennies = 0 | remainder = 0"))
+    end
+  end
+end
